0a2838fd7fef0225de9c394f8f3ae48cb4af631f
Ramon Iglesias <143233276+ramonigl@users.noreply.github.com>
2025-05-13 15:08:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const form = document.getElementById('form-atividade');
+  const formAddAtividade = document.getElementById('form-atividade');
+  const formAddCategoria = document.getElementById('adicionar-nova-categoria');
+  const btnNovaCategoria = document.getElementById('btn-nova-categoria');
   const chartContainer = document.getElementById('grafico-categorias');
 
+  const selectCategoriaExistente = document.querySelector('select#select-categoria');
+
+  const btnLimpar = document.getElementById('limpar-dados');
+  
   let chart;
+
+  function salvarCategoria(categoria) {
+    const categorias = JSON.parse(localStorage.getItem('categorias')) || [];
+    categorias.push(categoria);
+    localStorage.setItem('categorias', JSON.stringify(categorias));
+  }
+
+  function carregarCategorias() {
+    return JSON.parse(localStorage.getItem('categorias')) || [];
+  }
 
   function salvarAtividade(atividade) {
     const atividades = JSON.parse(localStorage.getItem('atividades')) || [];
@@ -34,7 +50,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (chart) chart.destroy();
 
     chart = new Chart(chartContainer, {
-      type: 'pie',
+      type: 'doughnut',
       data: {
         labels: dados.labels,
         datasets: [{
@@ -51,10 +67,10 @@ document.addEventListener('DOMContentLoaded', () => {
       options: {
         plugins: {
           legend: {
-            position: 'right'
+            display: false
           },
           title: {
-            display: true,
+            display: false,
             text: 'Tempo dedicado por categoria'
           }
         }
@@ -62,28 +78,90 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  form.addEventListener('submit', (e) => {
+  function atualizarSelectCategorias() {
+    //NO SELECT:
+    //remove antigas
+    selectCategoriaExistente.innerHTML = '';
+
+    const categorias = carregarCategorias();
+
+    //adiciona categorias existentes no select
+    categorias.forEach(cat => {
+      const option = document.createElement('option');
+      option.value = cat;
+      option.textContent = cat;
+      selectCategoriaExistente.appendChild(option);
+    });
+
+    //NO CATEGORIAS VISUAIS
+    const container = document.getElementById('categorias-visuais');
+
+    // Remove antigos
+    container.querySelectorAll('.categoria-btn').forEach(btn => btn.remove());
+
+    // Adiciona novos antes do botão +
+    categorias.forEach(cat => {
+      const btn = document.createElement('button');
+      btn.innerText = cat;
+      btn.classList.add('categoria-btn');
+
+      container.insertBefore(btn, btnNovaCategoria);
+    });
+
+  }
+
+  formAddCategoria.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const categoria = formAddCategoria['categoria'].value.trim();
+
+    if (categoria) {
+      const categorias = carregarCategorias();
+      if (categorias.includes(categoria)) {
+        alert('Essa categoria já existe.');
+        return;
+      }
+
+      salvarCategoria(categoria);
+      atualizarSelectCategorias();
+      
+      formAddCategoria.reset();
+      formAddCategoria.classList.toggle('hidden');
+      document.querySelector('#texto-btn-nova-categoria').classList.toggle('girar');
+    }
+  });
+
+  btnNovaCategoria.addEventListener('click', () => {
+    formAddCategoria.classList.toggle('hidden');
+    document.querySelector('#texto-btn-nova-categoria').classList.toggle('girar');
+    formAddCategoria.querySelector('input').focus();
+  });
+
+  formAddAtividade.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const atividade = {
-      nome: form['nome-atividade'].value,
-      categoria: form['categoria'].value,
-      tempo: form['tempo'].value,
-      data: form['data-atividade'].value
+      nome: formAddAtividade['nome-atividade'].value,
+      categoria: formAddAtividade['select-categoria'].value,
+      tempo: formAddAtividade['tempo'].value,
+      data: formAddAtividade['data-atividade'].value
     };
 
     salvarAtividade(atividade);
     renderizarGrafico();
-    form.reset();
-    form.querySelector('input').focus();
+    formAddAtividade.reset();
+    formAddAtividade.querySelector('input').focus();
   });
 
+  atualizarSelectCategorias();
   renderizarGrafico();
 
-  const btnLimpar = document.getElementById('limpar-dados');
   btnLimpar.addEventListener('click', () => {
     if (confirm('Tem certeza que deseja apagar todos os dados?')) {
       localStorage.removeItem('atividades');
+      localStorage.removeItem('categorias');
+
+      atualizarSelectCategorias();
       renderizarGrafico();
     }
   });
